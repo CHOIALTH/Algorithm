888bfdf1932f81591e7c8b400072d5921d62b310
Wonjun CHOI <107077825+CHOIALTH@users.noreply.github.com>
2023-04-08 01:38:14 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] Args){
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt(); // 불기
+        int x = 2541 - 1998;
+        System.out.println(y - x);
+    }
+}
