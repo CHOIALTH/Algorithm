e49c16665e9d4526e050f1b814af60c8a3eeeff5
Wonjun CHOI <107077825+CHOIALTH@users.noreply.github.com>
2023-04-22 23:58:31 +0900

    [Bronze V] Title: N 찍기, Time: 256 ms, Memory: 23420 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] Args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        Long num = Long.parseLong(st.nextToken());
+        
+        for(Long i = 1L; i <= num; i++){
+            bw.write(Long.toString(i)); // 
+            bw.newLine(); // 줄바꿈
+        }
+        bw.flush();
+        bw.close();
+    }
+}
