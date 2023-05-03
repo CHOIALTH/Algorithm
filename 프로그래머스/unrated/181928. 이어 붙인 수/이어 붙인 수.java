24df8f4ee09c19ce8dd3551def67b54c521f25bc
Wonjun CHOI <107077825+CHOIALTH@users.noreply.github.com>
2023-05-03 18:09:49 +0900

    [unrated] Title: 이어 붙인 수, Time: 15.70 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String x="", y="";
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                x += num_list[i];
+            }
+            else{
+                y += num_list[i];
+            }
+        }
+        answer = Integer.parseInt(x) + Integer.parseInt(y);
+        return answer;
+    }
+}
